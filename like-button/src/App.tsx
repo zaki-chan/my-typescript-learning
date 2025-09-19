b9336569178eaf71af63bbf6e6a4fc97dfe10b94
zaki-chan <naotti0926@gmail.com>
2025-09-19 23:21:16 +0900

    use react with ts

---
+++ like-button/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <LikeButton />
+    </>
+  )
+}
+
+function LikeButton(){
+  const [count, setCount] = useState(999);
+  const handleClick = () =>{
+    setCount(count + 1)
+  }
+  return <span className='likeButton' onClick={handleClick}>♥ {count}</span>
+}
+export default App
---
+++ src/ts/increment.ts
@@ -0,0 +1,4 @@
+function increment(num:number){
+    return num + 1;
+}
+console.log(increment(999));
